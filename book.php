00acd5ef8d25ce944fe0d50e82bd954b1691ec04
chriscodehead <chrischikabox@gmail.com>
2023-09-22 16:10:50 +0100

    New Update

---
+++ book.php
@@ -0,0 +1,160 @@
+<?php
+require_once('include.php');
+$title = $siteName . ' | ' . "From Doorstep to Destination, We`ve Got Your Back!";
+require_once('head.php') ?>
+
+<body>
+
+ <div class="preloader">
+  <div class="loader-ripple">
+   <div></div>
+   <div></div>
+  </div>
+ </div>
+
+ <?php require_once('header.php') ?>
+
+
+ <main class="main">
+
+  <!-- <div class="site-breadcrumb" style="background: url(assets/img/breadcrumb/01.jpg)">
+   <div class="container">
+    <h2 class="breadcrumb-title">Book A Ride</h2>
+    <ul class="breadcrumb-menu">
+     <li><a href="./">Home</a></li>
+     <li class="active">Book A Ride</li>
+    </ul>
+   </div>
+  </div> -->
+
+
+  <div class="book-ride py-120">
+   <div class="container">
+    <div class="row">
+     <div class="col-md-10 mx-auto">
+      <div class="booking-form">
+       <div class="book-ride-head">
+        <h4 class="booking-title">Make Your Booking Today</h4>
+        <p>Booking a ride with us is easy! You can call, book online, or use our website for seamless reservations.</p>
+       </div>
+       <form action="#">
+        <div class="row">
+         <div class="col-lg-4">
+          <div class="form-group">
+           <label>Full Name</label>
+           <input type="text" class="form-control" placeholder="Your Name">
+           <i class="far fa-user"></i>
+          </div>
+         </div>
+         <div class="col-lg-4">
+          <div class="form-group">
+           <label>Phone Number</label>
+           <input type="text" class="form-control" placeholder="Your Phone">
+           <i class="far fa-phone"></i>
+          </div>
+         </div>
+         <div class="col-lg-4">
+          <div class="form-group">
+           <label>Email</label>
+           <input type="text" class="form-control" placeholder="Your Email">
+           <i class="far fa-envelope"></i>
+          </div>
+         </div>
+         <div class="col-lg-6">
+          <div class="form-group">
+           <label>Pick Up Location</label>
+           <input type="text" class="form-control" placeholder="Type Location">
+           <i class="far fa-location-dot"></i>
+          </div>
+         </div>
+         <div class="col-lg-6">
+          <div class="form-group">
+           <label>Drop Off Location</label>
+           <input type="text" class="form-control" placeholder="Type Location">
+           <i class="far fa-location-dot"></i>
+          </div>
+         </div>
+         <div class="col-lg-6">
+          <div class="form-group">
+           <label>Passengers</label>
+           <input type="text" class="form-control" placeholder="Passengers">
+           <i class="far fa-user-tie"></i>
+          </div>
+         </div>
+         <div class="col-lg-6">
+          <div class="form-group">
+           <label>Cab Type</label>
+           <select class="select">
+            <option value>Choose Cab</option>
+            <option value="1">All Type</option>
+            <option value="2">Hybrid</option>
+            <option value="3">Luxury</option>
+           </select>
+          </div>
+         </div>
+         <div class="col-lg-6">
+          <div class="form-group">
+           <label>Pick Up Date</label>
+           <input type="text" class="form-control date-picker" placeholder="MM/DD/YY">
+           <i class="far fa-calendar-days"></i>
+          </div>
+         </div>
+         <div class="col-lg-6">
+          <div class="form-group">
+           <label>Pick Up Time</label>
+           <input type="text" class="form-control time-picker" placeholder="00:00 AM">
+           <i class="far fa-clock"></i>
+          </div>
+         </div>
+         <div class="col-lg-6">
+          <div class="form-group">
+           <label>Driver Age</label>
+           <select class="select">
+            <option value>Choose Age</option>
+            <option value="1">Any Age</option>
+            <option value="2">25</option>
+            <option value="3">30</option>
+            <option value="4">35</option>
+            <option value="5">40</option>
+           </select>
+          </div>
+         </div>
+         <div class="col-lg-6">
+          <div class="form-group">
+           <label>Cab Model</label>
+           <select class="select">
+            <option value>Choose Model</option>
+            <option value="1">All Model</option>
+            <option value="2">M5 2022</option>
+            <option value="3">Q7 2021</option>
+           </select>
+          </div>
+         </div>
+         <div class="col-lg-12">
+          <div class="form-group">
+           <label>Your Message</label>
+           <textarea class="form-control" rows="5" placeholder="Write Your Message"></textarea>
+          </div>
+         </div>
+         <div class="col-lg-12">
+          <div class="form-check">
+           <input class="form-check-input" type="checkbox" id="condition">
+           <label class="form-check-label" for="condition">
+            By using this form you agree to our terms & conditions.
+           </label>
+          </div>
+         </div>
+         <div class="col-lg-3 mx-auto">
+          <button class="theme-btn" type="submit">Book Your Taxi<i class="fas fa-arrow-right"></i></button>
+         </div>
+        </div>
+       </form>
+      </div>
+     </div>
+    </div>
+   </div>
+  </div>
+
+ </main>
+
+ <?php require_once('footer.php'); ?>
--- header.php
+++ header.php
@@ -1,59 +1,99 @@
  <header class="header">
-  <div class="header-top">
-   <div class="container">
-    <div class="header-top-wrapper">
-     <div class="header-top-left">
-      <div class="header-top-contact">
-       <ul>
-        <li><a href="mailto:<?php print $siteEmail; ?>"><i class="far fa-envelopes"></i>
-          <span><?php print $siteEmail; ?></span></a></li>
-        <li><a href="tel:<?php print $sitePhone; ?>"><i class="far fa-phone-volume"></i> <?php print $sitePhone; ?></a>
-        </li>
-        <li><a href="#"><i class="far fa-alarm-clock"></i> <?php print $siteAddress; ?></a></li>
-       </ul>
-      </div>
-     </div>
-     <div class="header-top-right">
-      <div class="header-top-link">
-       <!-- <a href="#"><i class="far fa-arrow-right-to-bracket"></i> Book</a>
+   <div class="header-top">
+     <div class="container">
+       <div class="header-top-wrapper">
+         <div class="header-top-left">
+           <div class="header-top-contact">
+             <ul>
+               <li><a href="mailto:<?php print $siteEmail; ?>"><i class="far fa-envelopes"></i>
+                   <span><?php print $siteEmail; ?></span></a></li>
+               <li><a href="tel:<?php print $sitePhone; ?>"><i class="far fa-phone-volume"></i> <?php print $sitePhone; ?></a>
+               </li>
+               <li><a href="#"><i class="far fa-alarm-clock"></i> <?php print $siteAddress; ?></a></li>
+             </ul>
+           </div>
+         </div>
+         <div class="header-top-right">
+           <div class="header-top-link">
+             <!-- <a href="#"><i class="far fa-arrow-right-to-bracket"></i> Book</a>
        <a href="#"><i class="far fa-user-vneck"></i> Register</a> -->
-      </div>
+           </div>
 
-      <div class="header-top-social">
-       <span>Follow Us: </span>
-       <a href="<?php print $siteFacebook; ?>"><i class="fab fa-facebook"></i></a>
-       <a href="<?php print $siteTwitter; ?>"><i class="fab fa-twitter"></i></a>
-       <a href="<?php print $siteInstagram; ?>"><i class="fab fa-instagram"></i></a>
-      </div>
+           <div class="header-top-social">
+             <span>Follow Us: </span>
+             <a href="<?php print $siteFacebook; ?>"><i class="fab fa-facebook"></i></a>
+             <a href="<?php print $siteTwitter; ?>"><i class="fab fa-twitter"></i></a>
+             <a href="<?php print $siteInstagram; ?>"><i class="fab fa-instagram"></i></a>
+           </div>
+         </div>
+       </div>
      </div>
-    </div>
    </div>
-  </div>
-  <div class="main-navigation">
-   <nav class="navbar navbar-expand-lg">
-    <div class="container position-relative">
-     <a class="navbar-brand" href="./">
-      <img src="img/logo.png" alt="logo">
-     </a>
-     <div class="mobile-menu-right">
-      <div class="search-btn">
-       <button type="button" class="nav-right-link"><i class="far fa-search"></i></button>
-      </div>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
-       <span class="navbar-toggler-mobile-icon"><i class="far fa-bars"></i></span>
-      </button>
-     </div>
-     <div class="collapse navbar-collapse" id="main_nav">
-      <ul class="navbar-nav">
-       <li class="nav-item"><a class="nav-link active" href="./">Home</a></li>
-       <li class="nav-item"><a class="nav-link" href="about">About</a></li>
-       <li class="nav-item"><a class="nav-link" href="book">Book Taxi</a></li>
-       <li class="nav-item"><a class="nav-link" href="services">Services</a></li>
-       <li class="nav-item"><a class="nav-link" href="contact">Contact</a></li>
-      </ul>
-     </div>
+   <div class="main-navigation">
+     <nav class="navbar navbar-expand-lg">
+       <div class="container position-relative">
+         <a class="navbar-brand" href="./">
+           <img src="img/logo.png" alt="logo">
+         </a>
+         <div class="mobile-menu-right">
+           <div class="search-btn">
+             <button type="button" class="nav-right-link"><i class="far fa-search"></i></button>
+           </div>
+           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
+             <span class="navbar-toggler-mobile-icon"><i class="far fa-bars"></i></span>
+           </button>
+         </div>
+         <div class="collapse navbar-collapse" id="main_nav">
+           <ul class="navbar-nav">
+             <li class="nav-item"><a class="nav-link active" href="./">Home</a></li>
+             <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+             <li class="nav-item"><a class="nav-link" href="#service">Service</a></li>
+             <li class="nav-item"><a class="nav-link" href="book">Book Taxi</a></li>
+             <li class="nav-item"><a class="nav-link" href="contact">Contact</a></li>
+           </ul>
+         </div>
 
-    </div>
-   </nav>
-  </div>
+       </div>
+     </nav>
+   </div>
  </header>
+
+
+ <div class="sidebar-popup">
+   <div class="sidebar-wrapper">
+     <div class="sidebar-content">
+       <button type="button" class="close-sidebar-popup"><i class="far fa-xmark"></i></button>
+       <div class="sidebar-logo">
+         <img src="img/logo.png" alt>
+       </div>
+       <div class="sidebar-about">
+         <h4>About Us</h4>
+         <p>Welcome to <?php print $siteName; ?>, your trusted transportation partner in Your City. We are proud to be the go-to choice for reliable, safe, and comfortable taxi services. At <?php print $siteName; ?>, we believe that every journey should be a hassle-free and enjoyable experience.</p>
+       </div>
+       <div class="sidebar-contact">
+         <h4>Contact Info</h4>
+         <ul>
+           <li>
+             <h6>Email</h6>
+             <a href="mailto:<?php print $siteEmail; ?>"><i class="far fa-envelope"></i><span><?php print $siteEmail; ?></span></a>
+           </li>
+           <li>
+             <h6>Phone</h6>
+             <a href="tel:+<?php print $sitePhone; ?>"><i class="far fa-phone"></i><?php print $sitePhone; ?></a>
+           </li>
+           <li>
+             <h6>Address</h6>
+             <a href="#"><i class="far fa-location-dot"></i><?php print $siteAddress; ?></a>
+           </li>
+         </ul>
+       </div>
+       <div class="sidebar-social">
+         <h4>Follow Us</h4>
+         <a href="<?php print $siteFacebook; ?>"><i class="fab fa-facebook"></i></a>
+         <a href="<?php print $siteTwitter; ?>"><i class="fab fa-twitter"></i></a>
+
+
+       </div>
+     </div>
+   </div>
+ </div>
